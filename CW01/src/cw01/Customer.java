8e2c009d200f3536e7e893a702d6a409bc00ce82
ItsVasconcellos <fernando.asv@uol.com.br>
2026-02-25 16:58:18 +0000

    feat: customer thread class

--- CW01/src/cw01/Customer.java
+++ CW01/src/cw01/Customer.java
@@ -8,11 +8,32 @@ package cw01;
  *
  * @author fvasconcellos
  */
-public class Customer {
-    
-    private 
-    
-    Customer(){
-        
+public class Customer extends Thread {
+    private String name;
+    private Buffet b;
+
+    Customer(Buffet buffet, String name) {
+        this.b = buffet;
+        this.name = name;
     }
+
+    @Override
+    public void run() {
+        try {
+            int action = (int) ((Math.random() * 6) + 1);
+            System.out.println("Action type:" + action);
+            if (action <= 5) {
+                b.takeOrder(action);
+            } else if (action == 6) {
+                b.piano.play();
+            } else {
+                Long music_length = (long) (1 + Math.random() * 100);
+                System.out.println("Listen to music for: " + music_length + "s");
+                Thread.sleep(music_length * 100);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrompida");
+        }
+    }
+
 }
